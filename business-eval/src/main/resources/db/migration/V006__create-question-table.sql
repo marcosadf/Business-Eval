546f92602b308fd594f9230e34d67093ed7bbfdc
Marcos Dourado <marcos.filho18@gmail.com>
2023-09-18 16:53:43 -0300

    Added the positive field in the question entity

--- business-eval/src/main/resources/db/migration/V006__create-question-table.sql
+++ business-eval/src/main/resources/db/migration/V006__create-question-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE question (
 	id SERIAL CONSTRAINT pk_id_question PRIMARY KEY,
 	description varchar(500),
 	position bigint,
-	connotation boolean NOT NULL,
+	positive boolean NOT NULL,
 	category_id bigint NOT NULL,
 	FOREIGN KEY (category_id) REFERENCES category (id)
 );
